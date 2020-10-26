90ac01a86c222828da3fbf081252973aa8a573d5
vanessamarely <vanessamarely@gmail.com>
2020-10-26 00:06:58 -0500

    Componente Drag and drop incluido en el modulo de material

--- platzi-board/src/app/board/list/list.component.ts
+++ platzi-board/src/app/board/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
--- platzi-board/src/app/material-cdk/material-cdk.module.ts
+++ platzi-board/src/app/material-cdk/material-cdk.module.ts
@@ -5,16 +5,21 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
+//CDK
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
+    DragDropModule,
   ],
   exports: [
     MatToolbarModule,
     MatIconModule,
+    DragDropModule,
   ]
 })
 export class MaterialCdkModule { }
