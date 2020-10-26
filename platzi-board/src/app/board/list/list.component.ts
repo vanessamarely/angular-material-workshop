743b3a1c385d63132456490404c7cb546a51af88
vanessamarely <vanessamarely@gmail.com>
2020-10-26 07:50:32 -0500

    elementos añadidos para limitar el espacio de arrastre

--- platzi-board/src/app/board/list/list.component.ts
+++ platzi-board/src/app/board/list/list.component.ts
@@ -14,4 +14,15 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
 }
