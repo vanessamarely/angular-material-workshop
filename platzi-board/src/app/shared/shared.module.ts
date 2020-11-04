861b7472836a3ef8e3cb46a7a6ac74cc00796250
Vanessa Aristizabal <vanessamarely@gmail.com>
2020-11-04 09:35:54 -0500

    Update shared.module.ts

--- platzi-board/src/app/shared/shared.module.ts
+++ platzi-board/src/app/shared/shared.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MaterialCdkModule } from "./../material-cdk/material-cdk.module";
-
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
