55734284aaf321e4fb0ad2c575588078c478ca17
Vanessa Aristizabal <vanessamarely@gmail.com>
2020-11-04 20:56:45 -0500

    Update material-cdk.module.ts

--- platzi-board/src/app/material-cdk/material-cdk.module.ts
+++ platzi-board/src/app/material-cdk/material-cdk.module.ts
@@ -8,6 +8,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 //CDK
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -24,6 +26,8 @@ import { TextFieldModule } from '@angular/cdk/text-field';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatTooltipModule,
     
     //cdk
@@ -38,6 +42,8 @@ import { TextFieldModule } from '@angular/cdk/text-field';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatTooltipModule,
     //cdk
     DragDropModule,
